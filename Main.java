b245589c0f027df3af4fa62f37352923ba2daf8a
RamirezC33 <00084020@uca.edu.sv>
2024-03-18 17:20:14 -0600

    :sparkles: Adding functions to calculator

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         int option = 0;
 
         do {
-            System.out.println("Calculator Menu:");
+            System.out.println("Menu:");
             System.out.println("1. Add");
             System.out.println("2. Subtract");
             System.out.println("3. Multiply");
@@ -32,10 +32,52 @@ public class Main {
                     System.out.println("Exit");
                     break;
                 default:
-                    System.out.println("Invalid choose a valid option");
+                    System.out.println("Invalid option");
                     break;
             }
         } while (option != 5);
 
         scanner.close();
     }
+
+    public static void Addition(Scanner scanner) {
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 + num2;
+        System.out.println("Your result is:  " + result);
+    }
+
+    public static void Subtraction(Scanner scanner) {
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 - num2;
+        System.out.println("Your result is:  " + result);
+    }
+
+    public static void Multiplication(Scanner scanner) {
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 * num2;
+        System.out.println("Your result is: " + result);
+    }
+
+    public static void Division(Scanner scanner) {
+        System.out.print("Enter the dividend: ");
+        double dividend = scanner.nextDouble();
+        System.out.print("Enter the divisor: ");
+        double divisor = scanner.nextDouble();
+
+        if (divisor == 0) {
+            System.out.println("Error: Division by zero");
+        } else {
+            double result = dividend / divisor;
+            System.out.println("Your result is: " + result);
+        }
+    }
+}
